33415c429b939a6514ceffb7b0f10980a09dc9d0
Daniel Otaviano <danielotavianobp@gmail.com>
2021-03-12 23:18:04 -0300

    feat: ensure post exist

--- server/service/post-service.js
+++ server/service/post-service.js
@@ -4,11 +4,17 @@ exports.getPosts = () => {
   return postsData.getPosts()
 }
 
-exports.getPost = (id) => {
-  return postsData.getPost(id)
+exports.getPost = async (id) => {
+  const post = await postsData.getPost(id)
+  if (!post) throw new Error('Post not found')
+
+  return post
 }
 
-exports.savePost = (post) => {
+exports.savePost = async (post) => {
+  const existingPost = await postsData.getPostByTitle(post.title)
+  if (existingPost) throw new Error('post already exists')
+
   return postsData.savePost(post)
 }
 
@@ -16,6 +22,9 @@ exports.deletePost = (id) => {
   return postsData.deletePost(id)
 }
 
-exports.updatePost = (id, post) => {
+exports.updatePost = async (id, post) => {
+
+  await exports.getPost(id)
+
   return postsData.updatePost(id, post)
 }
