ad0e461d6a7f0b19be8e2a9a9c2f949cd0f02cd8
Daniel Otaviano <danielotavianobp@gmail.com>
2021-03-12 21:53:47 -0300

    feat: create helper methods in post service

--- server/service/post-service.js
+++ server/service/post-service.js
@@ -3,3 +3,19 @@ const postsData = require('../data/posts-data')
 exports.getPosts = () => {
   return postsData.getPosts()
 }
+
+exports.getPost = (id) => {
+  return postsData.getPost(id)
+}
+
+exports.savePost = (post) => {
+  return postsData.savePost(post)
+}
+
+exports.deletePost = (id) => {
+  return postsData.deletePost(id)
+}
+
+exports.updatePost = (id, post) => {
+  return postsData.updatePost(id, post)
+}
