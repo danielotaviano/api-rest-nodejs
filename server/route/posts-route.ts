ae394d01ccdb96cf1c2e2076c4c4ec9ae01c357c
Daniel Otaviano <danielotavianobp@gmail.com>
2021-03-13 14:14:41 -0300

    refactor: instace postsService

--- server/route/posts-route.ts
+++ server/route/posts-route.ts
@@ -1,6 +1,13 @@
 import express from 'express'
-import * as postsService from '../service/post-service'
+import { PostsData } from '../data/posts-data'
+import { Post } from '../entities/post'
+import { generateDatabase } from '../infra/database'
+import { PostsService } from '../service/post-service'
 const router = express.Router()
+
+const database = generateDatabase<Post>()
+const postsData = new PostsData(database)
+const postsService = new PostsService(postsData)
 
 router.get('/posts', async (request, response, next) => {
   try {
