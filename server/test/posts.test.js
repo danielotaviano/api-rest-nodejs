22d67972efec5bca4d47d627358af645b3996eb8
Daniel Otaviano <danielotavianobp@gmail.com>
2021-03-12 23:18:31 -0300

    test: ensure post exist and change some axios config

--- server/test/posts.test.js
+++ server/test/posts.test.js
@@ -8,7 +8,7 @@ const generate = () => {
 }
 
 const request = async (url, method, data) => {
-  return axios({ url, method, data })
+  return axios({ url, method, data, validateStatus: false })
 }
 
 test('should get a posts', async () => {
@@ -17,6 +17,8 @@ test('should get a posts', async () => {
   const post3 = await postService.savePost({ title: generate(), content: generate() })
 
   const response = await request('http://localhost:3000/posts', 'get')
+
+  expect(response.status).toBe(200)
 
   const posts = response.data
 
@@ -31,6 +33,9 @@ test('should save a post', async () => {
   const data = ({ title: generate(), content: generate() })
 
   const response = await request('http://localhost:3000/posts', 'post', data)
+
+  expect(response.status).toBe(201)
+
   const post = response.data
 
   expect(post.title).toBe(data.title)
@@ -39,13 +44,29 @@ test('should save a post', async () => {
   await postService.deletePost(post.id)
 })
 
+test('should not save a post', async () => {
+  const data = ({ title: generate(), content: generate() })
+
+  const response1 = await request('http://localhost:3000/posts', 'post', data)
+  const response2 = await request('http://localhost:3000/posts', 'post', data)
+
+  expect(response2.status).toBe(409)
+
+  const post1 = response1.data
+
+  await postService.deletePost(post1.id)
+
+})
+
 test('should update a post', async () => {
   const post = await postService.savePost({ title: generate(), content: generate() })
 
   post.title = generate()
   post.content = generate()
 
-  await request(`http://localhost:3000/posts/${post.id}`, 'put', post)
+  const response = await request(`http://localhost:3000/posts/${post.id}`, 'put', post)
+
+  expect(response.status).toBe(204)
 
   const updatedPost = await postService.getPost(post.id)
 
@@ -54,11 +75,20 @@ test('should update a post', async () => {
 
   await postService.deletePost(post.id)
 })
+test('should not update a post', async () => {
+  const post = {
+    id: 1
+  }
+  const response = await request(`http://localhost:3000/posts/${post.id}`, 'put', post)
+  expect(response.status).toBe(404)
+})
 
 test('should delete a post', async () => {
   const post = await postService.savePost({ title: generate(), content: generate() })
 
-  await request(`http://localhost:3000/posts/${post.id}`, 'delete')
+  const response = await request(`http://localhost:3000/posts/${post.id}`, 'delete')
+
+  expect(response.status).toBe(204)
 
   const posts = await postService.getPosts()
 
